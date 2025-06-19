66cfaac4769d98b572910ba7f64d9baf6158876b
anirudh <anirudhjangid0@gmail.com>
2025-06-19 18:01:05 +0530

    Add user registration route and controller; integrate async error handling and update app structure to include static file serving.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -9,11 +10,15 @@ app.use(cors({
     credentials: true,
 }));
 
-
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static(path.join(process.cwd(), "public")));
 app.use(cookieParser());
+
+//routes imports
+import userRoutes from "./routes/user.routes.js";
+
+//routes
+app.use("/api/v1/users", userRoutes);
 
 export default app;
