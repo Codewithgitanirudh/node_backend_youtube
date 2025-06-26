6c5626e20a2f72d321c8969381fb46c9fad9bb2b
anirudh <anirudhjangid0@gmail.com>
2025-06-26 16:46:13 +0530

    Enhance user routes: add endpoints for current user retrieval, user updates, password changes, and avatar uploads; improve route organization and maintainability.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,32 @@
-import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
-import upload from "../middlewares/multer.middleware.js";
-import { verifyAccessToken } from "../middlewares/auth.middleware.js";
+import { Router } from 'express';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  getCurrentUser,
+  changePassword,
+  updateUser,
+  updateUserAvatar,
+  getUserChannels,
+  getWatchHistory,
+} from '../controllers/user.controller.js';
+import upload from '../middlewares/multer.middleware.js';
+import { verifyAccessToken } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
-router.route("/register").post(upload.fields([{name: "avatar"}, {name: "coverImage"}]), registerUser);
-router.route("/login").post(loginUser);
+router.route('/register').post(upload.fields([{ name: 'avatar' }, { name: 'coverImage' }]), registerUser);
+router.route('/login').post(loginUser);
 
 //secure routes
-router.route("/logout").post(verifyAccessToken, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken);
-router.route("/channels/:username").get(verifyAccessToken, getUserChannels);
+router.route('/logout').post(verifyAccessToken, logoutUser);
+router.route('/refresh-token').post(refreshAccessToken);
+router.route('/current-user').get(verifyAccessToken, getCurrentUser);
+router.route('/update-user').patch(verifyAccessToken, updateUser);
+router.route('/update-user-avatar').patch(verifyAccessToken, upload.single('avatar'), updateUserAvatar);
+router.route('/change-password').post(verifyAccessToken, changePassword);
+router.route('/channel/:username').get(verifyAccessToken, getUserChannels);
+router.route('/watch-history').get(verifyAccessToken, getWatchHistory);
 
 export default router;
